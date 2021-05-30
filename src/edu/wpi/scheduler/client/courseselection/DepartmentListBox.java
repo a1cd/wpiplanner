1acd11a9bbecd9e711addf121659e5767de0ba3e
Joshua McKeen <jdmckeen@wpi.edu>
2021-05-30 04:08:37 -0400

    kinda done i think

--- src/edu/wpi/scheduler/client/courseselection/DepartmentListBox.java
+++ src/edu/wpi/scheduler/client/courseselection/DepartmentListBox.java
@@ -41,8 +41,8 @@ public class DepartmentListBox extends ListBox {
 	private static final List<AcademicGroup> groups = Arrays.asList(
 			new AcademicGroup("Science", "MA", "PH", "AS", "BB", "BCB", "CH", "CS", "ES", "GE", "DS", "IMGD", "MMS", "MPE", "NEU"),
 			new AcademicGroup("Engineering", "ECE", "RBE", "AREN", "BME", "CE", "CHE", "ME", "MFE", "MTE", "NSE", "OIE", "FP", "SYS", "AE"),
-			new AcademicGroup("Language", "GN", "AB", "CN", "EN", "ESL", "SP"),
-			new AcademicGroup("Humanities", "PY", "SOC", "AR", "HI", "HU", "MU", "RE", "TH", "WR"),
+			new AcademicGroup("Language", "GN", "AB", "CN", "ESL", "SP"),
+			new AcademicGroup("Humanities", "PY", "SOC", "AR", "HI", "HU", "MU", "RE", "TH", "WR", "EN"),
 			new AcademicGroup("Social Science", "ECON", "SS", "PSY", "DEV", "ENV", "GOV", "INTL"),
 			new AcademicGroup("Business", "BUS", "ETR", "FIN", "MIS", "MKT", "OBC", "SD")
 			);
