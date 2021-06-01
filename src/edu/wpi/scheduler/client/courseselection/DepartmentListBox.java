69a8ad7a7398b81530e018f7db905bf128a878bb
Joshua McKeen <jdmckeen@wpi.edu>
2021-06-01 00:32:50 -0400

    adjusting subject list

--- src/edu/wpi/scheduler/client/courseselection/DepartmentListBox.java
+++ src/edu/wpi/scheduler/client/courseselection/DepartmentListBox.java
@@ -39,11 +39,11 @@ public class DepartmentListBox extends ListBox {
 	private Map<Department, OptionElement> options = new HashMap<Department, OptionElement>();
 
 	private static final List<AcademicGroup> groups = Arrays.asList(
-			new AcademicGroup("Science", "MA", "PH", "BB", "BCB", "CH", "CS", "ES", "GE", "DS", "IMGD", "MMS", "MPE", "MME", "NEU"),
-			new AcademicGroup("Engineering", "ECE", "RBE", "AREN", "BME", "CE", "CHE", "ME", "MFE", "MTE", "NSE", "OIE", "FP", "SYS", "AE"),
+			new AcademicGroup("Science", "MA", "PH", "BB", "BCB", "CH", "CS", "GE", "DS", "IMGD", "MMS", "MPE", "MME", "NEU"),
+			new AcademicGroup("Engineering", "ECE", "RBE", "AREN", "BME", "CE", "CHE", "ES", "ME", "MFE", "MTE", "NSE", "OIE", "FP", "SYS", "AE"),
 			new AcademicGroup("Language", "GN", "AB", "CN", "ESL", "ISE", "SP"),
-			new AcademicGroup("Humanities", "PY", "SOC", "AR", "HI", "HU", "MU", "RE", "TH", "WR", "EN"),
-			new AcademicGroup("Social Science", "ECON", "SS", "PSY", "DEV", "ENV", "GOV", "INTL", "SS", "SD"),
+			new AcademicGroup("Humanities", "PY", "SOC", "AR", "HI", "HU", "MU", "RE", "TH", "WR", "EN", "INTL"),
+			new AcademicGroup("Social Science", "ECON", "SS", "PSY", "DEV", "ENV", "GOV", "SS", "SD"),
 			new AcademicGroup("Business", "BUS", "ETR", "FIN", "MIS", "MKT", "OBC")
 			);
 
